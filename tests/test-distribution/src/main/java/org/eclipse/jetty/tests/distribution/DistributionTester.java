6aa10debe1f8a1a017a8e3f7b8b075d38cffe377
olivier lamy <oliver.lamy@gmail.com>
2021-03-24 12:22:50 +1000

    some more testing
    Signed-off-by: olivier lamy <oliver.lamy@gmail.com>

--- tests/test-distribution/src/main/java/org/eclipse/jetty/tests/distribution/DistributionTester.java
+++ tests/test-distribution/src/main/java/org/eclipse/jetty/tests/distribution/DistributionTester.java
@@ -150,6 +150,12 @@ public class DistributionTester
         List<String> commands = new ArrayList<>();
         commands.add(getJavaExecutable());
         commands.add("-Djava.io.tmpdir=" + workDir.toAbsolutePath().toString());
+        int debugPort = Integer.getInteger("distribution.debug.port", 0);
+        if (debugPort > 0)
+        {
+            commands.add("-Xdebug");
+            commands.add("-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=" + debugPort);
+        }
         commands.add("-jar");
         commands.add(config.jettyHome.toAbsolutePath() + "/start.jar");
 
