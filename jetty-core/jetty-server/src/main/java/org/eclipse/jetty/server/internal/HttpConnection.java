5490b3d7ed5b49ba268a9d57da9ca16ab44590b8
Simone Bordet <simone.bordet@gmail.com>
2025-09-21 22:36:13 +0200

    Experimental sketch for #13392.
    Signed-off-by: Simone Bordet <simone.bordet@gmail.com>

--- jetty-core/jetty-server/src/main/java/org/eclipse/jetty/server/internal/HttpConnection.java
+++ jetty-core/jetty-server/src/main/java/org/eclipse/jetty/server/internal/HttpConnection.java
@@ -943,7 +943,11 @@ public class HttpConnection extends AbstractMetaDataConnection implements Runnab
                                 getEndPoint().write(this, _content);
                                 break;
                             default:
-                                succeeded();
+                                Content.Source source = _info.getContentSource();
+                                if (source != null)
+                                    source.writeTo(getEndPoint(), source.getLength(), this);
+                                else
+                                    succeeded();
                         }
 
                         return Action.SCHEDULED;
@@ -1533,6 +1537,7 @@ public class HttpConnection extends AbstractMetaDataConnection implements Runnab
         @Override
         public void send(MetaData.Request request, MetaData.Response response, boolean last, ByteBuffer content, Callback callback)
         {
+            // TODO: do not rely on response==null.
             if (response == null)
             {
                 if (!last && BufferUtil.isEmpty(content))
